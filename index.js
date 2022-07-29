ff8bb49545408e7f8bdda6763f6586456f104308
Yuri Kozyk <y.kozyk@gmail.com>
2022-07-29 13:25:03 +0300

    add phone mask and change messages

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', function () {
   const alert = document.querySelector('.alert')
   const spinner = document.querySelector('.spinner')
+  const phoneInput = document.querySelector('#phoneNumber')
   const form = document.getElementById('register-form')
+
+  var phoneMask = IMask(phoneInput, {
+    mask: '+{38}(000)000-00-00',
+  })
+
   form.addEventListener('submit', function (e) {
     e.preventDefault()
     form.classList.add('d-none')
@@ -20,18 +26,23 @@ window.addEventListener('load', function () {
         })
         .then((data) => {
           console.log(data)
+          const footer = `<p>З одного номеру можна зареєструватись лише один раз.</p>
+          <p>Підпишіться на канал Telegram <a href="https://t.me/orikhiv_next" target="_blank">@orikhiv_next</a>, щоб дізнатись коли за вашим номером буде видаватись гуманітарна допомога.</p>
+          <p><img src="./img/orikhiv_next.jpg" alt="QR code" class="qr" /></p>`
           if (data.result === 'success') {
             alert.innerHTML = `<p>Ви успішно зареєструвались у черзі.</p>
             <p>Ваш номер: <strong style="font-size: 1.5em;">${data.row}</strong></p>
-            <p>Підпишіться на канал Telegram <a href="https://t.me/orikhiv_next" target="_blank">@orikhiv_next</a>, щоб дізнатись коли за вашим номером буде видаватись гуманітарна допомога.</p>
-            <p><img src="./img/orikhiv_next.jpg" alt="QR code" class="qr" /></p>`
+            ${footer}`
 
             alert.classList.remove('d-none')
+            alert.classList.add('alert-success')
             spinner.classList.add('d-block')
           }
 
           if (data.result === 'refused') {
-            alert.textContent = data.reason
+            alert.innerHTML = `<p>Відмова.</p>
+            <p>Користувач з номером телефону ${data.phoneNumber} вже був зареєстрований в черзі під номером: <strong>${data.number}</strong></p>
+            ${footer}`
             alert.classList.add('alert-warning')
             alert.classList.remove('d-none')
           }
